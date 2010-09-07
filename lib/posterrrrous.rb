fa3e0201054f65c7411a0aab7c03269fabcc1f9d
Joshua Nichols <josh@technicalpickles.com>
2010-09-07 18:15:44 -0400

    First impl.

---
+++ lib/posterrrrous.rb
@@ -0,0 +1,38 @@
+require 'httparty'
+require 'active_support/core_ext/array/extract_options'
+
+class Posterrrrous
+  include HTTParty
+
+  base_uri 'http://posterous.com/api'
+
+  class << self
+    attr_accessor :email, :password
+  end
+
+  def self.method_missing(method_sym, *arguments, &block)
+    attributes = arguments.pop
+
+    options = {
+      :query => attributes,
+      :basic_auth  =>  {
+        :username => email,
+        :password => password
+      }
+    }
+
+    response = get "/#{method_sym}", options
+    response = response["rsp"]
+
+    status = response.delete('stat')
+    if status != "ok"
+      error = response['err']
+      require 'ruby-debug'; breakpoint
+      raise "Posterous Error #{error['code']}: #{error['msg']}"
+    else
+      response.values.first
+    end
+  end
+
+
+end
